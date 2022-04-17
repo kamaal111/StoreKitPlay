2287eb3b790217de52c08bfba8ec2d4cf4f26d48
Kamaal <kamaal.f1@gmail.com>
2022-04-17 15:45:51 +0200

    Initial commit

---
+++ Shared/StoreKitPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKitPlayApp.swift
+//  Shared
+//
+//  Created by Kamaal M Farah on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct StoreKitPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
